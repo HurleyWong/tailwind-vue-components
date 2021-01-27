9805d55a44a4db1288eeb95e00dce6ed45936f38
Hurley <1401682479@qq.com>
2021-01-27 21:22:21 +0800

    🎉 初始化 Vue + Tailwind CSS 项目

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+/* eslint-disable no-unused-vars */
+import { createApp } from "vue";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+import "./assets/styles/tailwind.css";
+import router from "./router";
+
+createApp(App)
+  .use(router)
+  .mount("#app");
