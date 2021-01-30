6c86c1c10c9bee834b74bfaa0b4258eb6e91de4a
Hurley <1401682479@qq.com>
2021-01-30 15:31:52 +0800

    🛠 feat: 更新「忘记密码」界面

--- src/views/Reset.vue
+++ src/views/Reset.vue
@@ -1,67 +1,93 @@
 <template>
-  <div class="font-mono">
-    <!-- Container -->
-    <div class="container mx-auto">
-      <div class="flex justify-center px-6 my-12">
-        <!-- Row -->
-        <div class="w-full xl:w-3/4 lg:w-11/12 flex">
-          <!-- Col -->
-          <div
-            class="w-full h-auto bg-gray-400 hidden lg:block lg:w-1/2 bg-cover rounded-l-lg"
-            style="background-image: url('https://source.unsplash.com/oWTW-jNGl9I/600x800')"
-          ></div>
-          <!-- Col -->
-          <div
-            class="w-full lg:w-1/2 bg-gray-100 p-5 rounded-lg lg:rounded-l-none"
-          >
-            <div class="px-8 mb-4 text-center">
-              <h3 class="pt-4 mb-2 text-2xl">Forgot Your Password?</h3>
-              <p class="mb-4 text-sm text-gray-700">
-                Enter your email address below and we'll send you a link to
-                reset your password!
-              </p>
+  <div
+    class="min-h-screen min-w-full bg-gray-150 flex flex-col justify-center p-10"
+  >
+    <div
+      class="relative w-full max-w-full lg:max-w-6xl xl:max-w-screen-2xl mx-auto"
+    >
+      <div
+        class="absolute inset-0 -mr-3.5 bg-gradient-to-r from-red-100 to-purple-500 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:rotate-3 sm:rounded-3xl"
+      ></div>
+      <div class="relative bg-white shadow-lg sm:rounded-3xl">
+        <div class="flex items-center justify-start pt-6 pl-6">
+          <span class="w-3 h-3 bg-red-400 rounded-full mr-2"></span>
+          <span class="w-3 h-3 bg-yellow-400 rounded-full mr-2"></span>
+          <span class="w-3 h-3 bg-green-400 rounded-full mr-2"></span>
+        </div>
+
+        <div class="px-20 pt-8 pb-32">
+          <div class="font-mono">
+            <!-- Container -->
+            <div
+              class="min-w-screen bg-gray-150 flex items-center justify-center px-5 py-9"
+            >
+              <div
+                class="rounded-3xl shadow-xl w-full overflow-hidden"
+                style="max-width:1000px"
+              >
+                <!-- Row -->
+                <div class="md:flex w-full">
+                  <!-- Col -->
+                  <div
+                    class="py-10 px-10 w-1/2 md:block bg-gray-400 hidden lg:block lg:w-5/12 bg-cover rounded-l-lg"
+                    style="background-image: url('https://source.unsplash.com/oWTW-jNGl9I/600x800')"
+                  ></div>
+                  <!-- Col -->
+                  <div
+                    class="w-full lg:w-7/12 bg-gray-100 p-5 py-6 md:px-10 rounded-lg lg:rounded-l-none"
+                  >
+                    <div class="px-8 mb-1 text-center">
+                      <h3 class="pt-4 mb-2 text-2xl">Forgot Your Password?</h3>
+                      <p class="mb-4 text-sm text-gray-700">
+                        Enter your email address below and we'll send you a link
+                        to reset your password!
+                      </p>
+                    </div>
+                    <form class="px-8 pt-6 pb-8 mb-4 bg-gray-100 rounded">
+                      <div class="mb-4">
+                        <label
+                          class="block mb-2 text-sm font-bold text-gray-700"
+                          for="email"
+                        >
+                          Email
+                        </label>
+                        <input
+                          class="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                          id="email"
+                          type="email"
+                          placeholder="Enter Email Address..."
+                        />
+                      </div>
+                      <div class="mb-6 text-center">
+                        <button
+                          class="w-full px-4 py-2 font-bold text-white bg-red-500 rounded-full hover:bg-red-700 focus:outline-none focus:shadow-outline"
+                          type="button"
+                        >
+                          Reset Password
+                        </button>
+                      </div>
+                      <hr class="mb-6 border-t" />
+                      <div class="text-center">
+                        <router-link
+                          class="inline-block text-sm text-blue-500 align-baseline hover:text-blue-800"
+                          to="/signup"
+                        >
+                          Create an Account!
+                        </router-link>
+                      </div>
+                      <div class="text-center">
+                        <router-link
+                          class="inline-block text-sm text-blue-500 align-baseline hover:text-blue-800"
+                          to="/login"
+                        >
+                          Already have an account? Login!
+                        </router-link>
+                      </div>
+                    </form>
+                  </div>
+                </div>
+              </div>
             </div>
-            <form class="px-8 pt-6 pb-8 mb-4 bg-gray-100 rounded">
-              <div class="mb-4">
-                <label
-                  class="block mb-2 text-sm font-bold text-gray-700"
-                  for="email"
-                >
-                  Email
-                </label>
-                <input
-                  class="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-                  id="email"
-                  type="email"
-                  placeholder="Enter Email Address..."
-                />
-              </div>
-              <div class="mb-6 text-center">
-                <button
-                  class="w-full px-4 py-2 font-bold text-white bg-red-500 rounded-full hover:bg-red-700 focus:outline-none focus:shadow-outline"
-                  type="button"
-                >
-                  Reset Password
-                </button>
-              </div>
-              <hr class="mb-6 border-t" />
-              <div class="text-center">
-                <router-link
-                  class="inline-block text-sm text-blue-500 align-baseline hover:text-blue-800"
-                  to="/signup"
-                >
-                  Create an Account!
-                </router-link>
-              </div>
-              <div class="text-center">
-                <router-link
-                  class="inline-block text-sm text-blue-500 align-baseline hover:text-blue-800"
-                  to="/login"
-                >
-                  Already have an account? Login!
-                </router-link>
-              </div>
-            </form>
           </div>
         </div>
       </div>
--- src/views/Test.vue
+++ src/views/Test.vue
@@ -15,142 +15,82 @@
           <span class="w-3 h-3 bg-green-400 rounded-full mr-2"></span>
         </div>
 
-        <div class="px-20 py-6">
-          <!-- nav -->
-          <div class="flex items-center justify-between">
-            <div class="flex items-center justify-center">
+        <div class="px-20 pt-8 pb-32">
+          <div class="font-mono">
+            <!-- Container -->
+            <div
+              class="min-w-screen bg-gray-150 flex items-center justify-center px-5 py-9"
+            >
               <div
-                class="flex items-center justify-center text-3xl font-bold text-true-gray-800"
+                class="rounded-3xl shadow-xl w-full overflow-hidden"
+                style="max-width:1000px"
               >
-                <svg
-                  class="w-10 h-10 mr-1 text-blue-700"
-                  fill="none"
-                  stroke="currentColor"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M17.657 18.657A8 8 0 016.343 7.343S7 9 9 10c0-2 .5-5 2.986-7C14 5 16.09 5.777 17.656 7.343A7.975 7.975 0 0120 13a7.975 7.975 0 01-2.343 5.657z"
-                  ></path>
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M9.879 16.121A3 3 0 1012.015 11L11 14H9c0 .768.293 1.536.879 2.121z"
-                  ></path>
-                </svg>
-                Taliwind
+                <!-- Row -->
+                <div class="md:flex w-full">
+                  <!-- Col -->
+                  <div
+                    class="py-10 px-10 w-1/2 md:block bg-gray-400 hidden lg:block lg:w-5/12 bg-cover rounded-l-lg"
+                    style="background-image: url('https://source.unsplash.com/oWTW-jNGl9I/600x800')"
+                  ></div>
+                  <!-- Col -->
+                  <div
+                    class="w-full lg:w-7/12 bg-gray-100 p-5 py-6 md:px-10 rounded-lg lg:rounded-l-none"
+                  >
+                    <div class="px-8 mb-1 text-center">
+                      <h3 class="pt-4 mb-2 text-2xl">Forgot Your Password?</h3>
+                      <p class="mb-4 text-sm text-gray-700">
+                        Enter your email address below and we'll send you a link
+                        to reset your password!
+                      </p>
+                    </div>
+                    <form class="px-8 pt-6 pb-8 mb-4 bg-gray-100 rounded">
+                      <div class="mb-4">
+                        <label
+                          class="block mb-2 text-sm font-bold text-gray-700"
+                          for="email"
+                        >
+                          Email
+                        </label>
+                        <input
+                          class="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                          id="email"
+                          type="email"
+                          placeholder="Enter Email Address..."
+                        />
+                      </div>
+                      <div class="mb-6 text-center">
+                        <button
+                          class="w-full px-4 py-2 font-bold text-white bg-red-500 rounded-full hover:bg-red-700 focus:outline-none focus:shadow-outline"
+                          type="button"
+                        >
+                          Reset Password
+                        </button>
+                      </div>
+                      <hr class="mb-6 border-t" />
+                      <div class="text-center">
+                        <router-link
+                          class="inline-block text-sm text-blue-500 align-baseline hover:text-blue-800"
+                          to="/signup"
+                        >
+                          Create an Account!
+                        </router-link>
+                      </div>
+                      <div class="text-center">
+                        <router-link
+                          class="inline-block text-sm text-blue-500 align-baseline hover:text-blue-800"
+                          to="/login"
+                        >
+                          Already have an account? Login!
+                        </router-link>
+                      </div>
+                    </form>
+                  </div>
+                </div>
               </div>
             </div>
-            <div class="hidden md:flex items-center justify-center">
-              <router-link
-                to="/login"
-                class="mr-5 text-lg font-medium text-true-gray-800 hover:text-cool-gray-700 transition duration-150 ease-in-out"
-                >Login</router-link
-              >
-              <button
-                class="px-6 py-3 rounded-3xl font-medium bg-gradient-to-b from-gray-900 to-black text-white outline-none focus:outline-none hover:shadow-md hover:from-true-gray-900 transition duration-200 ease-in-out"
-              >
-                <router-link to="/register">Sign Up</router-link>
-              </button>
-            </div>
           </div>
-          <!-- /nav -->
-
-          <!-- hero section -->
-          <div class="lg:2/6 xl:w-2/4 mt-20 lg:mt-40 lg:ml-16 text-left">
-            <div class="text-6xl font-semibold text-gray-900 leading-none">
-              Hurley Huang
-            </div>
-            <div class="mt-6 text-xl font-light text-true-gray-500 antialiased">
-              <p class="font-semibold mb-5">Fresh graduated master at UoL</p>
-              <p>
-                Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec
-                odio. Quisque volutpat mattis eros. Nullam malesuada erat ut
-                turpis. Suspendisse urna nibh, viverra non, semper suscipit,
-                posuere a, pede.
-              </p>
-            </div>
-            <button
-              class="mt-6 px-8 py-4 rounded-full font-semibold tracking-wide bg-gradient-to-b from-blue-600 to-blue-700 text-white outline-none focus:outline-none hover:shadow-lg hover:from-blue-700 transition duration-200 ease-in-out"
-            >
-              Email me
-            </button>
-          </div>
-
-          <div class="lg:mt-14 lg:ml-20 text-left">
-            <bottom
-              type="button"
-              class="flex items-center justify-center w-12 h-12 rounded-full bg-cool-gray-100 text-gray-800 animate-bounce hover:text-gray-900 hover:bg-cool-gray-50 transition duration-300 ease-in-out cursor-pointer"
-            >
-              <!-- <svg
-                class="w-6 h-6"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M19 14l-7 7m0 0l-7-7m7 7V3"
-                ></path>
-              </svg> -->
-            </bottom>
-          </div>
-          <!-- /hero section -->
         </div>
       </div>
     </div>
   </div>
 </template>
-
-<script>
-// @ is an alias to /src
-// import HelloWorld from "@/components/HelloWorld.vue";
-
-export default {
-  name: "Home"
-};
-</script>
-
-<style>
-/* since nested groupes are not supported we have to use 
-     regular css for the nested dropdowns 
-  */
-li > ul {
-  transform: translatex(100%) scale(0);
-}
-li:hover > ul {
-  transform: translatex(101%) scale(1);
-}
-li > button svg {
-  transform: rotate(-90deg);
-}
-li:hover > button svg {
-  transform: rotate(-270deg);
-}
-
-/* Below styles fake what can be achieved with the tailwind config
-     you need to add the group-hover variant to scale and define your custom
-     min width style.
-  	 See https://codesandbox.io/s/tailwindcss-multilevel-dropdown-y91j7?file=/index.html
-  	 for implementation with config file
-  */
-.group:hover .group-hover\:scale-100 {
-  transform: scale(1);
-}
-.group:hover .group-hover\:-rotate-180 {
-  transform: rotate(180deg);
-}
-.scale-0 {
-  transform: scale(0);
-}
-.min-w-32 {
-  min-width: 8rem;
-}
-</style>
